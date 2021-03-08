8056465652a2e677324c76c309c4adc349204eb6
HamishD <hamish-drogemuller@live.nmit.ac.nz>
2021-03-09 11:04:18 +1300

    WeekThree Practice

--- src/WeekThree.js
+++ src/WeekThree.js
@@ -1,1 +1,27 @@
-console.log("Welcome")
+// TODO Javascript Variables
+
+// Declare a variable first name and assign a value string
+var firstName = 'Hamish'
+// Print It
+console.log(firstName)
+
+const lastName = 'Drogemuller'
+// Print It
+console.log(lastName)
+
+let fullName = firstName + ' ' + lastName //ES5
+
+fullName = `My full name is ${firstName} ${lastName}` //ES6
+
+let string = `Raid Shadow Legends`
+
+console.log(string)
+
+console.log(fullName)
+
+// Practice Variable Functions
+let V1 = 'Bobert'
+
+
+V1 = 'Dave'
+console.log(V1)
