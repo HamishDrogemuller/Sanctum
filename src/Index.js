1074f4598213a21b6190fc563b0fdbbf34226579
HamishD <hamish-drogemuller@live.nmit.ac.nz>
2021-03-10 22:16:49 +1300

    10.03.2021 Lesson commit

--- src/Index.js
+++ src/Index.js
@@ -1,7 +1,3 @@
-console.log("Welcome")
 
-function add(a,b) {
-    return a+b
-}
 
-var res = add(10, 11)
+
--- src/WeekThree.js
+++ src/WeekThree.js
@@ -25,3 +25,19 @@ let V1 = 'Bobert'
 
 V1 = 'Dave'
 console.log(V1)
+
+// 10.03.2021
+
+console.log('Hello')
+console.log('World ')
+
+function add(a, b) {
+    return a + b
+}
+
+console.log('Aaron where is your lab')
+
+let x = 10
+let sum = add(3, 2)
+
+console.log(sum)
