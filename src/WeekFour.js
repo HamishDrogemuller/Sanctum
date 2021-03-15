333d3d3025230353e7b9745191a1552b99e6ecb5
HamishD <hamish-drogemuller@live.nmit.ac.nz>
2021-03-16 09:37:19 +1300

    Lesson 16.03

---
+++ src/WeekFour.js
@@ -0,0 +1,43 @@
+
+let x;
+
+if (x < 0) {
+    console.log('Hamish')
+}
+else {
+   console.log('its false')
+}
+x = 10
+if (x === '10') {
+    console.log('this is true')
+} else {
+    console.log('error')
+}
+
+Number('123')
+Number('123') === 123
+
+Number("unicorn") 
+
+// Skeleton
+
+function name() {
+
+}
+
+//
+
+
+
+
+x = '123456'
+console.log(typeof x)
+let y = Number(x) 
+//console.log(Number(x))
+console.log(typeof y)
+
+const a = 1 + 1
+console.log(a)
+
+const b = a + 2
+console.log(b)
